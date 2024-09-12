9f0eb1967fe3b6089685b083cf13c2a26f9c6417
Kevin Clopp <kclopp@gmail.com>
2024-09-12 13:11:05 -0600

    Ex 70

--- basic1py.py
+++ basic1py.py
@@ -797,6 +797,18 @@ number_list.sort()
 print(number_list)
 '''
 
+# Ex70 Write a Python program to sort files by date. 
+'''
+import glob
+import os
+
+files = glob.glob("*.py")
+
+files.sort(key=os.path.getmtime)
+
+print("\n".join(files))
+'''
+
 
 
 
