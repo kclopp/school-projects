c9e989936a8706348d0fdcb6187579e8af01cb01
Kevin Clopp <kclopp@gmail.com>
2024-10-18 13:59:50 -0600

    Documentation

--- conditionStatementsLoops.py
+++ conditionStatementsLoops.py
@@ -2,6 +2,8 @@
 https://www.w3resource.com/python-exercises/python-conditional-statements-and-loop-exercises.php
 
 Python conditional statements and loops exercises
+
+Purpose: Learning.
 '''
 
 #1 Write a Python program to find those numbers which are divisible by 7 and multiple of 5, between 1500 and 2700 (both included).
