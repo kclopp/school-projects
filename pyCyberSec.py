6da1bab664387751e36baacbb7f2dfedfbf5c9c8
Kevin Clopp <kclopp@gmail.com>
2024-11-05 10:28:58 -0700

    CS # 3

--- pyCyberSec.py
+++ pyCyberSec.py
@@ -25,3 +25,25 @@ print(generate_password(int(input("Please input the length of the password: ")))
 print(generate_password())
 '''
 
+#3 Write a Python program to check if a password meets the following criteria:
+# At least 8 characters long and
+# Contains at least one uppercase letter, one lowercase letter, one digit, and one special character (!, @, #, $, %, or &)
+# If the password meets the criteria, print a message that says "Valid Password." If it doesn't meet the criteria, print a message that says "Password does not meet requirements."
+'''
+def passwordChecker(password):
+    import re
+    if len(password) < 8:
+        print("Password does not meet requirements.")
+    if not re.search("[a-z]", password):
+        print("Password does not meet requirements.")
+    if not re.search("[A-Z]", password):
+        print("Password does not meet requirements.")
+    if not re.search("[0-9]", password):
+        print("Password does not meet requirements.")
+    if not re.search("[!@#$%&]", password):
+        print("Password does not meet requirements.")
+    print("Password meets requirements.")
+
+passwordChecker(input("Please input your password: "))
+'''
+
